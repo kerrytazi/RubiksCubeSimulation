b7b3fa00c3cdfd1608f0cc877c459fdc4e25dfde
Alexander Akhundzhanov <kerrytazi@mail.ru>
2026-05-28 10:34:57 +0300

    Use custom panic handler for debug because of lldb internal problems

--- crates/app/src/main.rs
+++ crates/app/src/main.rs
@@ -1,6 +1,14 @@
-
-
 pub fn main() -> Result<(), Box<dyn std::error::Error>> {
+	#[cfg(debug_assertions)]
+	std::panic::set_hook(Box::new(|panic_info| {
+		eprintln!("Panic info: {}", panic_info);
+
+		#[cfg(target_arch = "x86_64")]
+		unsafe {
+			std::arch::asm!("int3");
+		}
+	}));
+
 	let simulation = simulation::SimulationWindow::new("Rubiks Cube Simulation")?;
 	simulation.window_loop();
 	Ok(())
